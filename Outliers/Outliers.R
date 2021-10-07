a4102cb8ae42ee4c6c16f8bb7592d87fa8992f90
datapower <dahee.kim@mnsu.edu>
2021-10-07 11:57:01 -0500

    Add files via upload

--- Outliers/Outliers.R
+++ Outliers/Outliers.R
@@ -2,6 +2,9 @@
 # Univariate approach
 ##############################
 # Method 1
+library(readxl)
+VCOMBCT <- read.csv(file = '~/VScode_project/R_projects/Data/VCOMBCT.csv')
+
 impute_outliers <- function(x,removeNA = TRUE){
   quantiles <- quantile( x, c(.05, .95 ),na.rm = removeNA )
   x[ x < quantiles[1] ] <- mean(x,na.rm = removeNA )
@@ -12,35 +15,25 @@ imputed_data <- impute_outliers(VCOMBCT$CT)
 par(mfrow = c(1, 2))
 boxplot(VCOMBCT$CT, main="Pressure Height having Outliers", boxwex=0.3)
 boxplot(imputed_data, main="Pressure Height with imputed data", boxwex=0.3)
+
 # Method 2 - Boxplot
-VCOMBCT <- read.csv(file = 'N:/DaheeKim/Pig_Trial/VCOMBCT.csv')
 out <- boxplot.stats(VCOMBCT$CT)$out
 out_ind <- which(VCOMBCT$CT %in% c(out))
 VCOMBCT[out_ind, ]
 # Method 2 - Same
-VCOMBCT <- read.csv(file = 'N:/DaheeKim/Pig_Trial/VCOMBCT.csv')
+library(rstatix)
 VCOMBCT %>%
   identify_outliers(CT)
 # Method 2 - Same
-VCOMBCT <- read.csv(file = 'N:/DaheeKim/Pig_Trial/VCOMBCT.csv')
 outliers <- boxplot(VCOMBCT$CT, plot=FALSE)$out
 VCOMBCT[which(VCOMBCT$CT %in% outliers),]
 
-
 # Method 3 - Percentiles
-VCOMBCT <- read.csv(file = 'N:/DaheeKim/Pig_Trial/VCOMBCT.csv')
-lower_bound <- quantile(VCOMBCT$CT, 0.01, na.rm=TRUE)
-upper_bound <- quantile(VCOMBCT$CT, 0.99, na.rm=TRUE)
-outlier_ind <- which(VCOMBCT$CT < lower_bound | VCOMBCT$CT > upper_bound)
-VCOMBCT[outlier_ind, ]
-
-VCOMBCT <- read.csv(file = 'N:/DaheeKim/Pig_Trial/VCOMBCT.csv')
 lower_bound <- quantile(VCOMBCT$CT, 0.05, na.rm=TRUE)
 upper_bound <- quantile(VCOMBCT$CT, 0.95, na.rm=TRUE)
 outlier_ind <- which(VCOMBCT$CT < lower_bound | VCOMBCT$CT > upper_bound)
 VCOMBCT[outlier_ind, ]
 nrow(VCOMBCT[outlier_ind, ])
-
 
 # Method 4 - Hampel filter
 lower_bound <- median(VCOMBCT$CT) - 3 * mad(VCOMBCT$CT)
@@ -53,12 +46,14 @@ VCOMBCT[outlier_ind, ]
 # Method 1 - CooksD
 model <- lm(COPIES ~ DAY|alival|trt, data = VCOMBCT)
 cooksd <- cooks.distance(model)
+#Plot
 plot(cooksd, pch="*", cex=2, main="Influential Obs by Cooks distance")  # plot cook's distance
 abline(h = 4*mean(cooksd, na.rm=T), col="red")
 abline(h = -4*mean(cooksd, na.rm=T), col="red")
 text(x=1:length(cooksd)+1, y=cooksd, labels=ifelse(cooksd>4*mean(cooksd, na.rm=T),names(cooksd),""), col="red")
 influential <- as.numeric(names(cooksd)[(cooksd > 4*mean(cooksd, na.rm=T))])  # influential row numbers
 VCOMBCT[influential, ]
+
 # Method 2 - Standar.Resid
 model <- lm(COPIES ~ DAY|alival|trt, data = VCOMBCT)
 model.metrics <- augment(model)
@@ -66,9 +61,12 @@ outlier <- model.metrics %>%
   filter(abs(.std.resid) > 3) %>%
   as.data.frame()
 VCOMBCT[outlier$.rownames,]
+
 # Method 3 - boxplot method
 VCOMBCT %>%
   group_by(DAY, alival, trt) %>%
+  identify_outliers(CT)
+
   identify_outliers(CT)
 ##############################
 # Multivariate approach - auto
